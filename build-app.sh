eefa5f9a354e2873172a84c5b55429621bad4a74
Eden <edem-2006@mail.ru>
2025-06-07 18:58:22 +0300

    add build script

---
+++ build-app.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+composer install --no-dev --optimize-autoloader
+npm install
+npm run build
+
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan migrate --force
